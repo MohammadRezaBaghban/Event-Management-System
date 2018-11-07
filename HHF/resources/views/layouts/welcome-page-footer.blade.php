8dab792c039609dc9fab419579f27afa15a41a05
SHEMA Gael <gagagael6@gmail.com>
2018-11-07 09:53:07 +0100

    Minor Changes

--- HHF/resources/views/layouts/welcome-page-footer.blade.php
+++ HHF/resources/views/layouts/welcome-page-footer.blade.php
@@ -1,5 +1,2 @@
-<script type="text/javascript" src="assets/js/jquery-2.1.4.min.js?ver=1"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
-<script type="text/javascript" src="assets/js/jquery.appear.js"></script>
-<script type="text/javascript" src="assets/js/jquery.plugin.js"></script>
